672ccf99120132894214a9b3a67d3d1a03a4b057
MarcusTheDestroyerNZ <sharkyandted@gmail.com>
2026-04-24 09:06:44 +1200

    Main Settup

---
+++ src/lib/components/counter.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    let count = $state(0)
+
+    function increment(){
+        count ++
+    }
+</script>
+
+<h1>{count}</h1>
+<button onclick={() => increment()}>Click</button>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+	import Counter from "$lib/components/counter.svelte";
+	import Test from "$lib/components/test.svelte";
+
+</script>
+
+
+<h1>Welcome to SvelteKit</h1>
+<p>hello, world!</p>
+<Test text="hi"/>
+<Counter />
